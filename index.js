2cf394ea9024a63bd178b71d7ebffd353914cc91
Prateek <notprateeek@gmail.com>
2021-01-01 13:42:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', () => {
-  const sounds = document.querySelectorAll('.sound');
+  const sounds = document.querySelectorAll('audio');
   const pads = document.querySelectorAll('.pads div');
-  const visual = document.querySelector('.visual');
   const colors = [
     '#60d394',
     '#d36060',
@@ -16,18 +15,6 @@ window.addEventListener('load', () => {
     pad.addEventListener('click', function () {
       sounds[index].currentTime = 0;
       sounds[index].play();
-      createBubbles(index);
     });
   });
-
-  //bubbles
-  const createBubbles = (index) => {
-    const bubble = document.createElement('div');
-    visual.appendChild(bubble);
-    bubble.style.backgroundColor = colors[index];
-    bubble.style.animation = 'jump 1s ease';
-    bubble.addEventListener('animationend', function () {
-      visual.removeChild(this);
-    });
-  };
 });
